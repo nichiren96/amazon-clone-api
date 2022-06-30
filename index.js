1068e7d691aa1e29d47ef9d8c993838097185d0b
Roel Tombozafy <rtombozafy96@gmail.com>
2022-06-30 11:09:57 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
+const PORT = process.env.PORT || 3000;
+const app = express();
+//const DB =
+("mongodb+srv://rivaan:rivaan123@cluster0.wpyhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+const DB = "mongodb://localhost:27017/flutter_amazon_clone";
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, () => {
+  console.log(`connected at port ${PORT}`);
+});
